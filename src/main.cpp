34b5c69ee22b8826093ff21aaeb690ecc87f325d
Compiler <LukeRocheDev@gmail.com>
2025-03-22 12:41:18 -0500

    Fix image view crashing

--- src/main.cpp
+++ src/main.cpp
@@ -125,15 +125,15 @@ private:
         std::cout << "Creating " << swapChainImageViews.size() << " image views\n";
 
         for (size_t i = 0; i < swapChainImages.size(); i++) {
-            VkImageViewCreateInfo createInfo;
+            VkImageViewCreateInfo createInfo{};
             createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
             createInfo.image = swapChainImages[i];
             createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
             createInfo.format = swapChainImageFormat;
-            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
-            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+            //createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            //createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            //createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            //createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
             // TODO: MipMap basexd on extent
             createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
             createInfo.subresourceRange.baseMipLevel = 0;
@@ -141,9 +141,11 @@ private:
             createInfo.subresourceRange.baseArrayLayer = 0;
             createInfo.subresourceRange.layerCount = 1;
 
+            std::cout << "Creating image view " << i+1 << std::endl;
             if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
                 throw std::runtime_error("failed to create image views!");
             }
+            std::cout << "Done!\n";
 
         }
 
