4977906963890d4223b894ff5cd272c2718c1041
Compiler <LukeRocheDev@gmail.com>
2025-03-22 12:35:29 -0500

    Image views are done

--- src/main.cpp
+++ src/main.cpp
@@ -86,6 +86,7 @@ private: // Client
     VkExtent2D swapChainExtent;
     VkColorSpaceKHR swapChainColorSpace;
     std::vector<VkImage> swapChainImages;
+    std::vector<VkImageView> swapChainImageViews;
 
 private:
 
@@ -114,9 +115,40 @@ private:
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapChain();
-
+        createImageViews();
 
     }
+
+    void createImageViews() {
+
+        swapChainImageViews.resize(swapChainImages.size());
+        std::cout << "Creating " << swapChainImageViews.size() << " image views\n";
+
+        for (size_t i = 0; i < swapChainImages.size(); i++) {
+            VkImageViewCreateInfo createInfo;
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[i];
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+            // TODO: MipMap basexd on extent
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+
+            if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create image views!");
+            }
+
+        }
+
+    }
+
     void createSwapChain() {
         SwapChainSupportDetails swapChainSupport = querySwapChainSupport(physicalDevice);
 
@@ -425,6 +457,9 @@ private:
     }
 
     void destroy() {
+        for (auto imageView : swapChainImageViews) {
+            vkDestroyImageView(device, imageView, nullptr);
+        }
         vkDestroySwapchainKHR(device, swapChain, nullptr);
         vkDestroyDevice(device, nullptr);
         if (enableValidationLayers) {
