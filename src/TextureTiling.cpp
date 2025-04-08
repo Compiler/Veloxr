8530f7ee2f4d3f7a0359db852166a8b4d0caa721
Compiler <LukeRocheDev@gmail.com>
2025-04-08 13:30:33 -0500

    Fix windows.

--- src/TextureTiling.cpp
+++ src/TextureTiling.cpp
@@ -131,7 +131,7 @@ TiledResult TextureTiling::tile8(OIIOTexture &texture, uint32_t deviceMaxDimensi
     int numThreads = std::min(totalTiles, 16);
     int tilesPerThread = (totalTiles + numThreads - 1) / numThreads;
 
-    OIIO::ImageCache *ic = OIIO::ImageCache::create(true);
+    auto ic = OIIO::ImageCache::create(true);
     ic->attribute("max_memory_MB", 1024.0f);
     OIIO::ImageSpec const *mainSpec = ic->imagespec(OIIO::ustring(texture.getFilename()), 0, 0);
     if (!mainSpec) {
