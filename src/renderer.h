c14e35cf2cf5a59fbf22baa4ece54b0d200257b4
Compiler <LukeRocheDev@gmail.com>
2025-04-23 14:41:24 -0500

    Adhoc cleanup and renewal from texture.

--- src/renderer.h
+++ src/renderer.h
@@ -180,8 +180,8 @@ public:
 
     void setTextureFilePath(std::string filepath) {
         _currentFilepath = filepath;
-        destroy();
-        run();
+        destroyTextureData();
+        setupTexturePasses();
 
     }
 
@@ -1508,8 +1508,8 @@ private:
     }
 
 public:
-    void destroy() {
 
+    void destroyTextureData() {
         cleanupSwapChain();
 
         for(auto& [name, data] : _textureMap) data.destroy(device);
@@ -1518,6 +1518,12 @@ public:
             vkDestroyBuffer(device, uniformBuffers[i], nullptr);
             vkFreeMemory(device, uniformBuffersMemory[i], nullptr);
         }
+        vkDestroyDescriptorPool(device, descriptorPool, nullptr);
+        vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
+    }
+    void destroy() {
+
+        destroyTextureData();
         vkDestroyDescriptorPool(device, descriptorPool, nullptr);
         vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
 
