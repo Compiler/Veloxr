9d042340ab06df14caf7a775787ad6a78778ce98
Josh yoo <josh.yoo@topazlabs.com>
2025-06-13 11:26:53 -0500

    fix quartz framework link issue

--- conan/mac_build_local.sh
+++ conan/mac_build_local.sh
@@ -1,10 +1,13 @@
 cd ..
 conan install conan/conanfile.py -of . \
     --version=0.0.1 \
+    --options="&:validation_layers=False" \
+    --options="&:build_photo=True" \
     -pr:h=conan/profile_mac_armv8 \
     -pr:b=conan/profile_mac_armv8
 
 cmake -S . -B build -G "Ninja" -DBUILD_TOOLS=True -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_TOOLCHAIN_FILE=build/Release/generators/conan_toolchain.cmake
+    -DCMAKE_TOOLCHAIN_FILE=build/Release/generators/conan_toolchain.cmake \
+    -DCMAKE_INSTALL_PREFIX=build/veloxr
 cmake --build build --target clean
-cmake --build build
+cmake --build build --target install
