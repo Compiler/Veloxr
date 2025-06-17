1e76af1402df8fdfe5dc8a4193cee90993e8a6b7
Compiler <LukeRocheDev@gmail.com>
2025-06-17 09:04:16 -0500

    Update layer

--- src/MetalSurfaceHelper.mm
+++ src/MetalSurfaceHelper.mm
@@ -1,14 +1,24 @@
 #import <Cocoa/Cocoa.h>
 #import <QuartzCore/CAMetalLayer.h>
 
-extern "C" void* GetMetalLayerForNSView(void* viewPtr)
-{
-    NSView* nsView = (__bridge NSView*)viewPtr;
-    if (!nsView.layer || ![nsView.layer isKindOfClass:[CAMetalLayer class]]) {
-        [nsView setWantsLayer:YES];
-        CAMetalLayer* metalLayer = [CAMetalLayer layer];
-        [nsView setLayer:metalLayer];
+extern "C" void *GetMetalLayerForNSView(void *viewPtr) {
+    NSView *nsView = (__bridge NSView *)viewPtr;
+
+    if ([nsView.layer isKindOfClass:[CAMetalLayer class]]) {
+        return (__bridge void *)nsView.layer;
     }
-    return (__bridge void*)nsView.layer; 
+
+    [nsView setWantsLayer:YES];
+
+    CAMetalLayer *metalLayer = [CAMetalLayer layer];
+    metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+    metalLayer.frame = nsView.bounds;
+    metalLayer.autoresizingMask = kCALayerWidthSizable | kCALayerHeightSizable;
+    metalLayer.contentsScale = nsView.window.backingScaleFactor;
+    metalLayer.drawableSize = CGSizeMake(nsView.bounds.size.width  * metalLayer.contentsScale, nsView.bounds.size.height * metalLayer.contentsScale);
+
+    [nsView.layer insertSublayer:metalLayer atIndex:0];
+
+    return (__bridge void *)metalLayer;
 }
 
