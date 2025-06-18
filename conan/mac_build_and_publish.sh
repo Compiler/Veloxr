bda5618baf3a0522843cdf0e9a9e70fa81c7ae4e
Josh yoo <josh.yoo@topazlabs.com>
2025-06-18 13:43:38 -0500

    fix bash

--- conan/mac_build_and_publish.sh
+++ conan/mac_build_and_publish.sh
@@ -9,20 +9,22 @@ rm -rf build
 
 python -m black conan/conanfile.py
 
-conan create conan/conanfile.py  \
-    --version=$VERSION \
-    --remote=topaz-conan \
-    --options="&:validation_layers=False" \
-    --options="&:build_photo=True" \
-    -pr:h=conan/profile_mac_armv8 \
-    -pr:b=conan/profile_mac_armv8
+for option in "build_video=True" "build_photo=True"; do
+    conan create conan/conanfile.py  \
+        --version=$VERSION \
+        --remote=topaz-conan \
+        --options="&:validation_layers=False" \
+        --options="&:$option" \
+        -pr:h=conan/profile_mac_armv8 \
+        -pr:b=conan/profile_mac_armv8
 
-conan create conan/conanfile.py  \
-    --version=$VERSION \
-    --options="&:validation_layers=False" \
-    --options="&:build_photo=True" \
-    --remote=topaz-conan \
-    -pr:h=conan/profile_mac_x86_64 \
-    -pr:b=conan/profile_mac_x86_64
+    conan create conan/conanfile.py  \
+        --version=$VERSION \
+        --options="&:validation_layers=False" \
+        --options="&:$option" \
+        --remote=topaz-conan \
+        -pr:h=conan/profile_mac_x86_64 \
+        -pr:b=conan/profile_mac_x86_64
 
-conan upload veloxr/$VERSION -r topaz-conan
+    conan upload veloxr/$VERSION -r topaz-conan
+done
